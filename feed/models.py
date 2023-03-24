3bea9bb41b3fdbe68167fa47ed6a759e75711240
tflaten4184 <tflaten4184@gmail.com>
2023-03-24 11:14:44 -0700

    add DateTimeField to Post and migrate

--- feed/models.py
+++ feed/models.py
@@ -2,3 +2,8 @@ from django.db import models
 
 class Post(models.Model):
     text = models.CharField(max_length=240)
+    date = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.text[0:100]
+    
