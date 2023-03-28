d8398763cc27c74adec9f49e7f894ad30726b11a
tflaten4184 <tflaten4184@gmail.com>
2023-03-28 11:40:10 -0700

    start detail view

--- feed/urls.py
+++ feed/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = "feed" # for namespacing urls
 
 urlpatterns = [
-    path("", views.HomePage.as_view(), name="index")
+    path("", views.HomePage.as_view(), name="index"),
+    path("<int:pk>/", views.PostDetailView.as_view(), name="detail"),
 ]
