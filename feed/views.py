d1aa2c18a825ea4e52132c522a821a958e759a4c
tflaten4184 <tflaten4184@gmail.com>
2023-03-29 10:07:14 -0700

    post creation form

--- feed/views.py
+++ feed/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from .models import Post
 
 class HomePage(ListView):
@@ -15,7 +17,21 @@ class PostDetailView(DetailView):
     model = Post
     context_object_name = "post"
 
-class CreatePostView(CreateView):
+class CreatePostView(LoginRequiredMixin, CreateView):
     template_name = "feed/create_post.html"
     model = Post
     fields = ["text"]
+    success_url = "/"
+
+    def dispatch(self, request, *args, **kwargs):
+        self.request = request
+        return super().dispatch(request, *args, **kwargs)
+    
+
+    def form_valid(self, form):
+
+        obj = form.save(commit=False)
+        obj.author = self.request.user
+        obj.save()
+        return super().form_valid(form)
+    
