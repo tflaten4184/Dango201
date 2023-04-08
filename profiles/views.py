344fbb03e1c8b635c3cd663976637ed0caa7102c
tflaten4184 <tflaten4184@gmail.com>
2023-04-08 10:27:26 -0700

    total followers on profile

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,8 @@
+from typing import Any, Dict
 from django.contrib.auth.models import User
 from django.views.generic import DetailView
+
+from feed.models import Post
 
 class ProfileDetailView(DetailView):
     http_method_names = ["get"]
@@ -8,3 +11,12 @@ class ProfileDetailView(DetailView):
     context_object_name = "user" # for alias in template (user.username instead of object.username)
     slug_field = "username"
     slug_url_kwarg = "username"
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        user = self.get_object()
+        context =  super().get_context_data(**kwargs)
+        context["total_posts"] = Post.objects.filter(author=user).count()
+        # TODO: Total Followers
+        # context["total_followers"] = Follower.objects.filter(subject?=user).count() # Replace subject with the field we end up using in Follower
+
+        return context
